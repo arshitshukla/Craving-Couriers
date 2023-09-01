1860d637946c19a075e5a8a8a9c6e4f53e9868e3
Arshit Shukla <109364576+arshitshukla@users.noreply.github.com>
2023-09-01 22:14:41 +0530

    Update SignupForm.js

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -8,7 +8,7 @@ const SignupForm = () => {
     const handlesubmit= async (e)=>{
         e.preventDefault();
         const {name,email,password,location}=credentials;
-        const response= await fetch("http://localhost:5000/api/createuser",{
+        const response= await fetch("https://cravingcouriers.onrender.com/api/createuser",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
